eb177c5df4a60b10a2410a4948e60572bfd50050
Afia <arminakterafia@gmail.com>
2025-10-21 15:20:25 +0600

    practice

--- src/layout/Search.jsx
+++ src/layout/Search.jsx
@@ -1,44 +1,59 @@
 import React from "react";
-import vector from '../assets/Vector.png';
+import vector from "../assets/Vector.png";
+// import ut from "../assets/Hadis/ut.png";
 
 const Search = () => {
   return (
-    <section className="container mx-auto w-full h-[200px] mt-10 relative rounded-xl bg-[#17B686] flex justify-center items-center px-3">
+    <section className="container mx-auto w-full h-[200px] mt-10 relative rounded-xl bg-[#17B686] flex justify-center items-center px-3 overflow-hidden">
       
-   
+      {/* left mosque */}
       <img
         src={vector}
         alt="vector"
-        className="absolute left-1 top-4/5 w-5/7 md:top-4/5 md:left-4    -translate-y-1/2 h-full md:w-1/5 object-contain"
+        className="absolute left-1 bottom-0 w-1/5 md:w-1/6 object-contain opacity-90"
       />
 
+      {/* right camel */}
+      {/* <img
+        src={ut}
+        alt="camel"
+        className=" origin-center  absolute bottom-0 right-4 w-1/5 md:w-1/6 opacity-90 z-[2]"
+      /> */}
 
-
-
-      {/* div for input + icon + button */}
-      <div className="flex w-full md:w-1/2 border-2 border-[#38A47E] rounded-lg overflow-hidden relative z-10">
-        
-        {/* icon */}
-        <div className="flex items-center px-3 bg-white">
+      {/* search bar */}
+      <div className="flex w-full md:w-1/2 border-2 border-[#38A47E] rounded-lg overflow-hidden relative z-10 bg-white shadow-md">
+        <div className="flex items-center px-3">
           <i className="fa-solid fa-magnifying-glass text-gray-500 text-lg"></i>
         </div>
 
-        {/* input */}
         <input
           type="text"
           placeholder="হাদিস খুঁজুন..."
-          className="flex-1 p-2 md:p-3 bg-white text-gray-900 outline-none"
+          className="flex-1 p-2 md:p-3 text-gray-900 outline-none"
         />
 
-        {/* search button */}
-        <div className="py-1 px-1 bg-white">
-          <button className="bg-[#38A47E] text-white border-2 py-2 px-5 rounded-xl mr-4">
+        <div className="py-1 px-1">
+          <button className="bg-[#38A47E] text-white border-2 py-2 px-5 rounded-xl mr-2 hover:bg-[#2d8f6a] transition">
             Search
           </button>
         </div>
-
       </div>
-    </section>
+
+      {/* ✅ Right side curved background (no SVG)
+      <div className="absolute bottom-0 right-0 bg-white w-[200px] h-[120px] md:w-[290px] md:h-[90px] rounded-tl-[100%] z-[1] rotate-300"></div>
+      <div className="origin-center rotate-45"></div> */}
+
+
+   
+
+  {/* অন্যান্য কোড */}
+  
+ 
+ 
+
+</section>
+
+    
   );
 };
 
